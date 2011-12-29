951a8f521d0bcfd7c314c2eb6a32ee5cac3756fd
Justin Mui <jmui@YPCMC10137.corp.yp.com>
2011-12-29 11:17:47 -0800

    - allow functionality to add zip codes and add leading zeros to small   zips

--- zip_code_lookup.rb
+++ zip_code_lookup.rb
@@ -5,7 +5,22 @@ class ZipCode
   end
 
   def find zip_code
-    @zip_code_lookup[zip_code]
+    @zip_code_lookup[zip_code.to_s.rjust(5, '0')]
+  end
+
+  def add zip_code, options = {}
+    false if options["city"].blank?      || options["latitude"].blank? ||
+             options["longitude"].blank? || options["county"].blank?
+
+    @zip_code_lookup[zip_code] = options
+  end
+
+  def to_yaml
+    File.open(File.join(Rails.root, "zip_code_lookup.yml"), "w") do |f|
+      f.write(@zip_code_lookup.to_yaml)
+    end
+
+    true
   end
 end
 
